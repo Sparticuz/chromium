48b9273bfb8043483e25b688dfc9553de0e6db7c
Sparticuz <kyle@kmcnally.net>
2023-02-10 07:20:11 -0500

    Add follow-redirects
    Will probably switch to node-fetch when this project goes to ESM

--- source/helper.ts
+++ source/helper.ts
@@ -1,7 +1,13 @@
 import { unlink } from "node:fs";
-import { get } from "node:https";
+import { https } from "follow-redirects";
 import { tmpdir } from "node:os";
-import { extract } from 'tar-fs';
+import { extract } from "tar-fs";
+import { parse } from "node:url";
+import type { UrlWithStringQuery } from "node:url";
+
+interface FollowRedirOptions extends UrlWithStringQuery {
+  maxBodyLength: number;
+}
 
 export const isValidUrl = (input: string) => {
     try {
@@ -13,9 +19,11 @@ export const isValidUrl = (input: string) => {
 
 export const downloadAndExtract = async (url: string) =>
     new Promise<string>((resolve, reject) => {
+        const getOptions = parse(url) as FollowRedirOptions;
+        getOptions.maxBodyLength = 60 * 1024 * 1024; // 60mb
         const destDir = `${tmpdir()}/chromium-pack`
         const extractObj = extract(destDir)
-        get(url, (response) => {
+        https.get(url, (response) => {
             response.pipe(extractObj);
             extractObj.on('finish', () => {
                 resolve(destDir);
