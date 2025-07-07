2184bb70318b0e868d9f1e8ced73360318103709
Sparticuz <kyle@kmcnally.net>
2025-07-07 10:21:49 -0400

    Do the same as esm instead of shortcutting

--- source/paths.cjs.ts
+++ source/paths.cjs.ts
@@ -1,9 +1,9 @@
-import { join } from "node:path";
+import { dirname, join } from "node:path";
 
 /**
  * Get the bin directory path for CommonJS modules
  */
 export function getBinPath(): string {
   // eslint-disable-next-line unicorn/prefer-module
-  return join(__dirname, "..", "bin");
+  return join(dirname(__filename), "..", "bin");
 }
